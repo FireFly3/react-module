470bc8613ac9f5e4cdb2298434b1c5cb3aefcaa5
FireFly3 <4irelly@gmail.com>
2022-01-12 17:06:29 +0200

    class work react

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -16,18 +16,3 @@ const Comment = ({comment}) => {
 
 export default Comment;
 
-// import React from 'react';
-//
-// const Post = ({post}) => {
-//     return (
-//         <div>
-//             <div>userId: {post.userId}</div>
-//             <div>id: {post.id}</div>
-//             <div>title: {post.title}</div>
-//             <div>body: {post.body}</div>
-//         </div>
-//     );
-// };
-//
-// export default Post;
-
--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -21,26 +21,3 @@ const Comments = () => {
 
 export default Comments;
 
-// import React, {useEffect, useState} from 'react';
-
-// import Post from "../Post/Post";
-//
-// const Posts = () => {
-//     const [posts, setPosts] =useState([]);
-//
-//     useEffect(() => {
-//         fetch('https://jsonplaceholder.typicode.com/posts')
-//             .then(value => value.json())
-//             .then(value => setPosts(value))
-//     }, []);
-//
-//     return (
-//         <div>
-//             {
-//                 posts.map(value => <Post key={value.id} post={value}/>)
-//             }
-//         </div>
-//     );
-// };
-//
-// export default Posts;
