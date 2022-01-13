b0c9ac5d1d7a5916089a99c52b9c8f3b80f07d38
FireFly3 <4irelly@gmail.com>
2022-01-13 19:29:40 +0200

    HW2

---
+++ src/components/User/User.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const User = ({user}) => {
+    return (
+        <div>
+            id: {user.id}
+            <br/>
+            name: {user.name}
+            <br/>
+            username: {user.username}
+        </div>
+    );
+};
+
+export default User;
+
+
+// import React from 'react';
+//
+// import './User.css'
+//
+// const User = ({user}) => {
+//     return (
+//         <div className="userStyle">
+//             <div>id: {user.id}</div>
+//             <div>name: {user.name}</div>
+//             <div>username: {user.username}</div>
+//             <div>email: {user.email}</div>
+//             <div>
+//                 Address:
+//                 <div>street: {user.address.street}</div>
+//                 <div>suite: {user.address.suite}</div>
+//                 <div>city: {user.address.city}</div>
+//                 <div>zipcode: {user.address.zipcode}</div>
+//                 <div>Geo:
+//                     <div>lat: {user.address.geo.lat}</div>
+//                     <div>lng: {user.address.geo.lng}</div>
+//                 </div>
+//             </div>
+//             <div>phone: {user.phone}</div>
+//             <div>website: {user.website}</div>
+//             <div>company:
+//                 <div>name: {user.company.name}</div>
+//                 <div>catchPhrase: {user.company.catchPhrase}</div>
+//                 <div>bs: {user.company.bs}</div>
+//             </div>
+//         </div>
+//     );
+// };
+//
+// export default User;
