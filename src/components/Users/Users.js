b799a699b97a27768ac674931900669b6aabe453
FireFly3 <4irelly@gmail.com>
2022-01-17 16:15:16 +0200

    HW2

---
+++ src/components/Users/Users.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from 'react';
+
+const Users = () => {
+    const[users, setUsers ] = useState([]);
+
+    useEffect()
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Users;
