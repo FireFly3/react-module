a1fca562d9b72b7d050fd8cdc1ff96cb75cf3625
FireFly3 <4irelly@gmail.com>
2022-01-27 21:19:12 +0200

    HW4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import './App.css';
+import {useReducer} from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'inc':
+      return {...state, count1: state.count1 + 1}
+    case 'dec':
+      return {...state, count1: state.count1 - 1}
+    case 'reset':
+      return {...state, count1: action.payload}
+    case 'inc1':
+      return {...state, count2: state.count2 + 1}
+    case 'dec1':
+      return {...state, count2: state.count2 - 1}
+    case 'reset1':
+      return {...state, count2: action.payload}
+    case 'inc2':
+      return {...state, count3: state.count3 + 1}
+    case 'dec2':
+      return {...state, count3: state.count3 - 1}
+    case 'reset2':
+      return {...state, count3: action.payload}
+    default:
+      throw new Error('MyError')
+  }
+  return state
+}
+
 
 function App() {
-  return (
-    <div>
+  const [state, dispatch] = useReducer(reducer, {count1: 0, count2:0, count3:0});
 
-    </div>
+
+  return (
+      <div >
+        <div>
+          <div>{state.count1}</div>
+          <button onClick={() => dispatch({type: 'inc'})}>INC</button>
+          <button onClick={() => dispatch({type: 'dec'})}>DEC</button>
+          <button onClick={() => dispatch({type: 'reset', payload: 0})}>RESET</button>
+        </div>
+        <div>
+          <div>{state.count2}</div>
+          <button onClick={() => dispatch({type: 'inc1'})}>INC</button>
+          <button onClick={() => dispatch({type: 'dec1'})}>DEC</button>
+          <button onClick={() => dispatch({type: 'reset1', payload: 0})}>RESET</button>
+        </div>
+        <div>
+          <div>{state.count3}</div>
+          <button onClick={() => dispatch({type: 'inc2'})}>INC</button>
+          <button onClick={() => dispatch({type: 'dec2'})}>DEC</button>
+          <button onClick={() => dispatch({type: 'reset2', payload: 0})}>RESET</button>
+        </div>
+      </div>
   );
 }
 
